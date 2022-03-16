beafa8df3bf18a1bd65f4a7127e465c2830e76a8
Vitor Silvestri <vitorsilvestri1@gmail.com>
2022-03-16 19:01:48 -0300

    adiciona site estático

---
+++ docs/_next/static/9va_AnxW8hdqBaO5rHHWv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/114-328959822bcba0f9.js","static/chunks/pages/index-30d06d9993b08555.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
