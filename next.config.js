79b50af0a23299b126e6004c78c979b000f794d0
Vitor Silvestri <vitorsilvestri1@gmail.com>
2022-03-16 21:22:43 -0300

    adiciona ícone e título

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './docs'
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,15 @@ import { Group } from '@mantine/core';
 import { BikeForm } from '../components/BikeForm';
 import { BurpeeForm } from '../components/BurpeeForm';
 import { SwimForm } from '../components/SwimForm';
+import Head from 'next/head';
 
 export default function Index() {
     return (
         <>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <title>Desafio ALJR</title>
+            </Head>
             <Header />
             <Group direction="column" px="xl" spacing="xl" align="stretch" grow>
                 <BurpeeForm />
