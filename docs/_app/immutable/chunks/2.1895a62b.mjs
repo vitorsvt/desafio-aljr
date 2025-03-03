a9e91566af707d8d2c8fb0a2f664ddb6e3467264
Vitor Silvestri <vitor@silvestri.dev>
2025-03-03 12:56:17 -0300

    update github pages

---
+++ docs/_app/immutable/chunks/2.1895a62b.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.1d803437.mjs";export{t as component};
--- docs/_app/immutable/chunks/2.f7868513.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.1d6c7810.mjs";export{t as component};
