a1ca9adc5ed6c0618903976e8ba3ed97260e463a
Vitor Silvestri <vitor@silvestri.dev>
2025-11-09 12:57:08 -0300

    atualiza desafio para natal 2025

---
+++ docs/_app/immutable/chunks/2.525815ee.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.15907973.mjs";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,29 +5,7 @@
 	import RunCalculator from '../components/calculator/RunCalculator.svelte';
 	import BikeCalculator from '../components/calculator/BikeCalculator.svelte';
 	import Section from '../components/Section.svelte';
-	import ExtraPoints from '../components/ExtraPoints.svelte';
 </script>
-
-<Section title="O Desafio">
-	<p>
-		O desafio é uma competição, sobretudo
-		consigo mesmo, sendo você o seu principal adversário na luta contra maus hábitos e
-		buscando os substituir por melhores.
-	</p>
-
-	<p>
-		Este é um desafio individual e exclusivo aos alunos ALJR, sendo um dos objetivos
-		o de melhorar a disciplina, compromisso e consistência de todos os alunos em relação
-		aos treinos de artes marciais, motivando-os a aumentar a frequência nas aulas, bem
-		como incentivar a realizar outras atividades, como corrida, pedalada, natação, etc.
-	</p>
-
-	<p>
-		Cada treino feito ou atividade proposta realizada vale uma determinada
-		pontuação. Ao final do desafio, haverá premiação especial aos três primeiros
-		colocados e a todos que alcançarem a meta de pontuação do desafio.
-	</p>
-</Section>
 
 <Section title="Corrida / Caminhada">
 	<RunCalculator />
@@ -48,5 +26,3 @@
 <Section title="Natação">
 	<SwimCalculator />
 </Section>
-
-<ExtraPoints />
